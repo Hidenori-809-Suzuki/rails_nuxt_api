b7f9337fcbc666dc8681f95745e9aae1ac71d03b
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2024-04-27 07:46:29 +0900

    Create Users_Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      # api test action
-      resources :hello, only:[:index]
+      # users_controller
+      resources :users, only:[:index]
     end
   end
 end
